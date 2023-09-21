a013f360d85565ca876e6fa41a26b54ee0656f08
rauldangio <jojofagqwrt@gmail.com>
2023-09-21 09:01:14 -0300

    fazer o input

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/logo.png';
+import style from './Header.module.scss';
+
+export default function Header(){
+  return (
+    <div>
+         <header className={style.header}>
+        <div className={style.header_content}>
+          <img src={logo} alt="logo" className={style.icone}/>
+          <div className={style.ancoras}>
+            <a href="/#">Quem somos</a>
+            <a href="/#">Cardapio</a>
+            <a href="/#">Nossos produtos</a>
+            <a href="/#">Delivery</a>
+          </div>
+        </div>
+      </header>
+    </div>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,17 @@
-import logo from '../../assets/logo.png';
-import mainProduct from '../../assets/dogao.jfif';
-import style from './Home.module.scss';
 
+import Header from '../../components/Header';
+import style from './Home.module.scss';
 
 export default function Home(){
   return (
     <div>
-      <header className={style.header}>
-        <img src={logo} alt="logo" />
-        <div>
-          <a href="/#">Quem somos</a>
-          <a href="/#">Cardapio</a>
-          <a href="/#">Nossos produtos</a>
-          <a href="/#">Delivery</a>
-        </div>
-      </header>
-
       <div>
-        <img src={mainProduct} alt="promoção do dia" />
+        <Header />
       </div>
 
+      <div className={style.main_product}>
+          <h1>Os Melhores Cachorros Quentes com Molho Chilli</h1>
+      </div>
     </div>
 
   )
